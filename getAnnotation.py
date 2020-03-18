c90f1ac48621373668c369b6dfd61b1f4ae032aa
stoneager <huan.qiu.bio@gmail.com>
2020-03-18 19:54:22 -0400

    add mandatory argument group

--- getAnnotation.py
+++ getAnnotation.py
@@ -9,38 +9,43 @@ import pandas as pd
 import shutil, errno
 import module.utility as utility
 
-def get_qry_data(qry, df_data, retain_all):
+def get_query_data(qryfile, data, keep_all):
     """
     Retrieve data for a list of queries from a dataframe
     
     Arguments:
-        qry: a list of queries with security names at the first column. If more than 
-             one columns, the file is tab delimited
-        df_data: a pandas dataframe database from which information will be retrieved
-        retain_all: boolean. If true, securities without match in database will not be
-             discarded.
+        qryfile: a file containing list of queries with security names at the first 
+            column. If more than one columns present, the file should be tab delimited.
+        data: a pandas dataframe containing information to be retrieved. The index is a
+            list of security name
+        keep_all: a boolean. If true, query securities not present in 'data' will not be
+            discarded. Instead, their attributes are filled with '0'
              
     Return:
-        a subset of the dataframe database containing rows for query securites
+        a dataframe with rows representing query securites
     """
     
     # set up empty dataframe and rows    
     df=pd.DataFrame()
     df.index.name="Symbol"
-    empty_row = pd.Series('0', df_data.columns)
+    empty_row = pd.Series('0', data.columns)
     
     # read query file, loop through and append rows to empty dataframe
-    q = open(qry,"r")
+    try:
+        queries = open(qryfile,"r")
+    except OSError:
+        print (f"fail to open/read file {qryfile}")
+        
     hit = 0
-    for line in q:
-        symbol=line.rstrip().split("\t")[0]
+    for line in queries:
+        symbol=line.rstrip().split("\t")[0]     # security name is at the 1st column
         if symbol == "Symbol":
             continue
-        if symbol in df_data.index:
-            df = df.append(df_data.loc[symbol], ignore_index=False)
+        if symbol in data.index:
+            df = df.append(data.loc[symbol], ignore_index=False)
             hit +=1
         else:
-            if retain_all:
+            if keep_all:
                 empty_row.name = symbol
                 df = df.append(empty_row, ignore_index=False)
     print ( "{}/{} output securities have annotations".format(hit, str(len(df))) )
@@ -48,6 +53,15 @@ def get_qry_data(qry, df_data, retain_all):
     return df
 
 def sort_growth_value(df):
+    """
+    Sort dataframe by growth score and by value score
+
+    Arguments:
+        df: a pandas dataframe
+    
+    Return:
+        df: a pandas dataframe after sorting
+    """
     if "Growth Score" in df and "Value Score" in df:
         df["Growth Score"].replace('0','X',inplace=True)
         df["Value Score"].replace('0','X',inplace=True)
@@ -64,19 +78,21 @@ def sort_growth_value(df):
 if __name__ == "__main__":
 
     # set up argument parser
-    text= ("Given a list of securites, retrieve their descriptive information from a "
-           "pandas dataframe database")
+    text= ("Given list(s) of securites, retrieve their descriptive information from a "
+           "local database")
     parser = argparse.ArgumentParser(description = text)
-    
-    parser.add_argument("queries",nargs='*',
-                                  help=": files containing a list of securities")
-                                  
-    parser.add_argument("--data", help=": a file containing the descriptive database")
-    parser.add_argument("--dlmt", help=": specify if the database is delimited by 'comma'"
+                              
+    mandatory = parser.add_argument_group('mandatory arguments')
+    mandatory.add_argument("qryfiles",nargs='*',
+                                  help=": file(s) containing a list of securities")
+    mandatory.add_argument("--data", help=": a file containing security attribute data")
+
+    optional = parser.add_argument_group('more optional arguments')
+    optional.add_argument("--dlmt", help=": specify if the database is delimited by 'comma'"
                                        " or tab. Default: comma",
                                   default = "comma")
-    parser.add_argument("--all" , help=": retain all securities regardless of availability"
-                             " of their information in database",
+    optional.add_argument("--all" , help=": retain all securities regardless of availability"
+                                       " of their information in database",
                                   action='store_true')
 
     # get parser ready
@@ -85,24 +101,29 @@ if __name__ == "__main__":
     
     # population key parameters
     data_file = args.data
+
+    keep_all = False
+    if args.all: keep_all = True
+
     delimiter = args.dlmt
-    if not delimiter: delimiter = "comma"
-    retain_all = False
-    if args.all: retain_all = True
+    if not delimiter or delimiter== "comma": 
+        delimiter = "\,"
+    elif delimiter == "tab":
+        delimiter = "\t"
+    else:
+        print ("invalid delimiter {}\n".format(delimiter) )
+        parser.print_help(sys.stderr)
+        sys.exit(1)
     
     # load database file to dataframe and set 'Symbol' column as index
     data=pd.DataFrame()
     if os.path.exists(data_file):
-        # read database file
-        if  delimiter == "comma":
-            data=pd.read_csv(data_file)
-        elif delimiter == "tab":
-            data=pd.read_csv(data_file, sep="\t")
-        else:
-            print ("invalid delimiter {}\n".format(delimiter) )
-            parser.print_help(sys.stderr)
-            sys.exit(1)
-            
+        try:
+            data=pd.read_csv(data_file, sep=delimiter)
+        except:
+            print (f"Pandas error with openning {data_file}, ", sys.exc_info()[0])
+            raise
+                        
         # use ticker column as replacement of symbol column
         if "Ticker" in data and "Symbol" not in data:
             data["Symbol"] = data["Ticker"]
@@ -111,16 +132,11 @@ if __name__ == "__main__":
         if "Symbol" in data.columns:
             data=data.set_index("Symbol")
         else:
-            print ("No 'Symbol' column found in database file")
+            print (f"No 'Symbol' column found in database file {data_file}")
             sys.exit(1)
         
     # loop through query files, data retrieval and write output file
-    for query in args.queries:
-        # retrieve data
-        df = pd.DataFrame()
-        df = get_qry_data(query, data, retain_all)
-        
-        # sort data by growth and value score (if these scores are available)
+    for query in args.qryfiles:
+        df = get_query_data(query, data, keep_all)
         df = sort_growth_value(df)
-        # write data to output file
         df.to_csv(query+".txt", sep="\t")
